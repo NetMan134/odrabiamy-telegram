62fbfbd8c962ddd4e503143282f64dbc80e565d2
NetMan <13informatyka14@gmail.com>
2022-10-03 23:06:12 +0200

    currently in middle of fixing images

--- odrabiamy-bot-telegram.py
+++ odrabiamy-bot-telegram.py
@@ -30,57 +30,41 @@ def getOdrabiamyToken(email, password):
         return odrabiamyToken
     except:
         return False
-
 # odrabiamy - assign token and check login details or premium subscription
 odrabiamyToken = getOdrabiamyToken(odrabiamyLogin, odrabiamyPass)
 if odrabiamyToken == False:
-    print('Incorrect login details for odrabiamy.\nOr perhaps you don\'t have premium?')
+    print('\nIncorrect login details for odrabiamy.\nOr perhaps you don\'t have premium?\n(Or no internet connection?)')
     quit()
-
-
-
+print(odrabiamyToken)
 # odrabiamy - download page
 def downloadPage():
     requestPageGet = requests.get(url=f'https://odrabiamy.pl/api/v2/exercises/page/premium/{pageNo}/{bookId}',
         headers={'user-agent':'new_user_agent-huawei-144','Authorization': f'Bearer {odrabiamyToken}'}).content.decode('utf-8')
-    listOfData = json.loads(requestPageGet).get('data')
-    global bookName, exerciseNo, fullHtml, writeHtmlToFile
-        
+    jsonData = json.loads(requestPageGet).get('data')
+    global bookName, jsonExerciseNo, writeHtmlToFile
     forExercise = forExercises = 0
-    jsonExercisesDict = {}
-    for exercise in listOfData:
-        jsonExercisesDict[exercise['id']] = exercise['number']
+    for exercise in jsonData:
         forExercise += 1
-    bookName = listOfData[forExercises]['book']['name']
-    writeHtmlToFile = f'<head><meta charset="UTF-8"></head><h1 style=\"font-weight:700;color:#200;font-family:\'Arial\',sans-serif;\">{bookName}</h1><br>'
+    # bookName = jsonData[forExercises]['book']['name']
+    writeHtmlToFile = f'<head><meta charset="UTF-8"></head><h1 style=\"font-weight:700;color:#200;font-family:\'Arial\',sans-serif;\">{bookName}</h1>'
     while forExercises < forExercise:
-        exerciseNo = listOfData[forExercises]['number']
-        exerciseId = listOfData[forExercises]['id']
-        solution = listOfData[forExercises]['solution']
-        solutionParser = BeautifulSoup(solution, 'html.parser')
-
-        for svgSmallObject in solutionParser.find_all('object', class_='small', type='image/svg+xml'):
+        jsonExerciseNo = jsonData[forExercises]['number']
+        jsonExerciseId = jsonData[forExercises]['id']
+        gottenSolution = jsonData[forExercises]['solution']
+        parsedSolution = BeautifulSoup(gottenSolution, 'html.parser')
+        for svgSmallObject in parsedSolution.find_all('object', class_='small', type='image/svg+xml'):
             svgSmallObject.extract()
-
-        for svgMathSmallObject in solutionParser.find_all('object', class_='math small', type='image/svg+xml'):
+        for svgMathSmallObject in parsedSolution.find_all('object', class_='math small', type='image/svg+xml'):
             svgMathSmallObject.extract()
-
         if not os.path.exists(f'{path}/{bookName}-{bookId}/{pageNo}'):
             os.makedirs(f'{path}/{bookName}-{bookId}/{pageNo}')
         
-        writeHtmlToFile += f'<div class="exercise-{exerciseId}"><h1 style=\"font-weight:700;color:#200;font-family:\'Arial\',sans-serif;\">Zadanie {exerciseNo}, Strona {pageNo}</h1>{solutionParser}</div><br>'
-
+        writeHtmlToFile += f'<div class="exercise-{jsonExerciseId}"><h1 style=\"font-weight:700;color:#200;font-family:\'Arial\',sans-serif;\">Zadanie {jsonExerciseNo}, Strona {pageNo}</h1>{parsedSolution}</div><br>'
         forExercises += 1
-    jsonExercisesObject = json.dumps(jsonExercisesDict, indent=forExercise)
-    with open(f'{path}/{bookName}-{bookId}/{pageNo}/exercises.json', 'a+') as jsonExercisesFileOut:
-        jsonExercisesFileOut.write(jsonExercisesObject)
-
+    
     fileInside = open(f'{path}/{bookName}-{bookId}/{pageNo}/index.html', 'a+', encoding='utf-8')
     fileInside.write(writeHtmlToFile)
     fileInside.close()
-    global requestLocal
-    requestLocal = 'Request'
-
 
 # telegram - /start command
 def start(update: Update, context: CallbackContext):
@@ -103,91 +87,54 @@ def pomoc(update: Update, context: CallbackContext):
          + "```https://odrabiamy.pl/nazwa\-przedmiotu/ksiazka\-id/strona\-numer```\n\(lub tak jak w komendzie /zadanie\)"
          , parse_mode='MarkdownV2')
 
-
-
-def pageDiskCmd(update: Update, context: CallbackContext):
+def processCmd(update: Update, context: CallbackContext):
     if 'odrabiamy.pl' in update.message.text:
-        global bookId, pageNo, exerciseId, user, requestLocal, bookName
+        global bookId, pageNo, exerciseId, user, bookName
+
+        user = update.message.from_user
+        print('User \"{}\" (id: {}) requested:'.format(user['username'], user['id']))
+        
         urlArguments = update.message.text.split('odrabiamy.pl')[1].split(' ')[0].split('/')
-        subject = urlArguments[1]        
+        subject = urlArguments[1]
         bookId = urlArguments[2].split('-')[1]
         pageNo = urlArguments[3].split('-')[1]
-        user = update.message.from_user
-        print('User \"{}\" (id: {}) requested page OPEN of:'.format(user['username'], user['id']))
-        print("subject: " + subject + "\nbookId: " + bookId + "\npageNo: " + pageNo)
-        update.message.reply_text("Przedmiot: " + subject + "\nIdentyfikator książki: " + bookId + "\nStrona: " + pageNo)
+        try:
+            exerciseId = urlArguments[4].split('-')[1]
+        except IndexError:
+            exerciseId = 'null'
+        print(f'subject: {subject}\nbookId: {bookId}\npageNo: {pageNo}\nexerciseId: {exerciseId}')
+        update.message.reply_text(f'Przedmiot: {subject}\nIdentyfikator książki: {bookId}\nStrona: {pageNo}\nIdentyfikator zadania: {exerciseId}')
+
         bookGetRequest = requests.get(url=f'https://odrabiamy.pl/api/v3/books/{bookId}').content.decode('utf-8')
-        listBook = json.loads(bookGetRequest)
-        if listBook.get('name') == None:
+        if json.loads(bookGetRequest).get('name') == None:
             print('Wrong bookId\n')
             return
-        
-        bookName = listBook['name']
+        else:
+            bookName = str(json.loads(bookGetRequest).get('name'))
+
         if not os.path.exists(f'{path}/{bookName}-{bookId}/{pageNo}'):
             print('Requesting download from server...\n')
             downloadPage()
+            requestOrLocal = 'R'
         else:
             print('Opening from local...\n')
-            requestLocal = 'Local'
-        
-        # global openFile
-        openFile = open(f'{path}/{bookName}-{bookId}/{pageNo}/index.html', 'r', encoding='utf-8').read()
-
-        async def generatePng():
-            browser = await launch(handleSIGINT=False, handleSIGTERM=False, handleSIGHUP=False)
-            newPage = await browser.newPage(); await newPage.goto("data:text/html;charset=utf-8," + openFile, {'waitUntil':'networkidle0'})
-            time.sleep(0.5)
-            global finalPngOutput
-            finalPngOutput = await newPage.screenshot({'fullPage': 'true'})
-            time.sleep(0.5)
-            await browser.close()
-
-        runGeneratePng = asyncio.new_event_loop()
-        asyncio.set_event_loop(runGeneratePng)
-        print('Starting PNG Capture...')
-        runGeneratePng.run_until_complete(generatePng())
-        print('Completed PNG Capture')
-        context.bot.send_document(update.effective_chat.id,
-            BytesIO(finalPngOutput),filename=f"{requestLocal}_{bookName}_Strona_{pageNo}.png")
-
-def pageExerciseDiskCmd(update: Update, context: CallbackContext):
-    if 'odrabiamy.pl' in update.message.text:
-        global bookId, pageNo, exerciseId, user, requestLocal, bookName
-        urlArguments = update.message.text.split('odrabiamy.pl')[1].split(' ')[0].split('/')
-        subject = urlArguments[1]        
-        bookId = urlArguments[2].split('-')[1]
-        pageNo = urlArguments[3].split('-')[1]
-        exerciseId = urlArguments[4].split('-')[1]
-        user = update.message.from_user
-        print('User \"{}\" (id: {}) requested page EXERCISE OPEN of:'.format(user['username'], user['id']))
-        print("subject: " + subject + "\nbookId: " + bookId + "\npageNo: " + pageNo)
-        update.message.reply_text("Przedmiot: " + subject + "\nIdentyfikator książki: " + bookId + "\nStrona: " + pageNo + "\nIdentyfikator zadania: " + exerciseId)
-        bookGetRequest = requests.get(url=f'https://odrabiamy.pl/api/v3/books/{bookId}').content.decode('utf-8')
-        listBook = json.loads(bookGetRequest)
-        if listBook.get('name') == None:
-            print('Wrong bookId\n')
-            return
-        
-        bookName = listBook['name']
-        if not os.path.exists(f'{path}/{bookName}-{bookId}/{pageNo}'):
-            print('Requesting download from server...\n')
-            downloadPage()
-        else:
-            print('Opening from local...\n')
-            requestLocal = 'Local'
+            requestOrLocal = 'L'
         
         openFile = open(f'{path}/{bookName}-{bookId}/{pageNo}/index.html', 'r', encoding='utf-8').read()
 
         soupFromFile = BeautifulSoup(openFile, 'html.parser')
-        getExerciseFromFile = str(soupFromFile.find('div', class_=f'exercise-{exerciseId}').extract())
+        if exerciseId != 'null':
+            getFileContents = f'<head><meta charset="UTF-8"></head><h1 style=\"font-weight:700;color:#200;font-family:\'Arial\',sans-serif;\">{bookName}</h1>' + str(soupFromFile.find('div', class_=f'exercise-{exerciseId}').extract())
+        else:
+            getFileContents = str(soupFromFile)
 
         async def generatePng():
             browser = await launch(handleSIGINT=False, handleSIGTERM=False, handleSIGHUP=False)
-            newPage = await browser.newPage(); await newPage.goto("data:text/html;charset=utf-8," + getExerciseFromFile, {'waitUntil':'networkidle0'})
-            time.sleep(0.5)
-            global finalPngOutput
+            newPage = await browser.newPage()
+            # await newPage.goto("data:text/html;charset=utf-8," + getFileContents, {'waitUntil':'networkidle0'})
+            await newPage.goto("data:text/html;charset=utf-8," + getFileContents, {'waitUntil':'domcontentloaded'})
+            time.sleep(4); global finalPngOutput
             finalPngOutput = await newPage.screenshot({'fullPage': 'true'})
-            time.sleep(0.5)
             await browser.close()
 
         runGeneratePng = asyncio.new_event_loop()
@@ -196,23 +143,17 @@ def pageExerciseDiskCmd(update: Update, context: CallbackContext):
         runGeneratePng.run_until_complete(generatePng())
         print('Completed PNG Capture')
         context.bot.send_document(update.effective_chat.id,
-            BytesIO(finalPngOutput),filename=f"{requestLocal}_{bookName}_Strona_{pageNo}.png")
-
+            BytesIO(finalPngOutput),filename=f"{requestOrLocal}_{bookName}_Strona_{pageNo}.png")
 
 # telegram - handle commands and redirect to functions
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('pomoc', pomoc))
-# updater.dispatcher.add_handler(CommandHandler('exercise', page
-updater.dispatcher.add_handler(CommandHandler('exercise', pageExerciseDiskCmd))
-updater.dispatcher.add_handler(CommandHandler('zadanie', pageExerciseDiskCmd))
-updater.dispatcher.add_handler(CommandHandler('page', pageDiskCmd))
-updater.dispatcher.add_handler(CommandHandler('strona', pageDiskCmd))
+updater.dispatcher.add_handler(CommandHandler('get', processCmd))
 
 # telegram - start bot
 updater.start_polling()
 print('\nBot started successfully')
 
 # odrabiamy - downloaded html (temporary - TO-DO: do it better and remove this)
-fullHtml = ' '
 writeHtmlToFile = ' '
